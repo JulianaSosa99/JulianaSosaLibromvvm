4f0bb1fca8afd76a69624052354c780117986ee0
Julianasosa99 <juliana.sosa@udla.edu.ec>
2024-06-17 10:53:33 -0500

    Creacion de LibroService y IlibroService , queme datos en la clse normal con el metodo que definimos dentro de la interfaz

---
+++ Services/ILibroService.cs
@@ -0,0 +1,18 @@
+﻿using JulianaSosaLibromvvm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JulianaSosaLibromvvm.Services
+{
+    public  interface ILibroService
+    //el objetivo es desacoplar el codigo
+    {
+        List<Libro> ObtenerLibro();
+        
+
+
+    }
+}
---
+++ Services/LibroService.cs
@@ -0,0 +1,30 @@
+﻿using JulianaSosaLibromvvm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JulianaSosaLibromvvm.Services
+{
+    public class LibroService : ILibroService
+    {
+        public List<Libro> ObtenerLibro()
+
+        {
+            return new List<Libro>
+            {
+
+
+            new Libro { Titulo = "La La Land", Autor = "Shonda Rimes" },
+            new Libro { Titulo = "El rey Leon", Autor = "Dani Mora" },
+            new Libro { Titulo = "Hunger Games", Autor = "Pri Zuñiga" },
+            new Libro { Titulo = "Bridgenton", Autor = "Pepe Velez" },
+            new Libro { Titulo = "Game of thrones", Autor = "Kamy Cardenas" }
+
+            };
+
+
+        }
+    }
+}
